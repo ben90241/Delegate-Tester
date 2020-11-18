6d8e1ee6e0a5af464c8c95feb81d9c0352de3727
Benson <Benson@DESKTOP-H3UQDON>
2020-11-18 00:59:09 -0800

    remove test member method

--- DelegateTester/Program.cs
+++ DelegateTester/Program.cs
@@ -7,10 +7,6 @@ namespace DelegateTester
         public delegate void MyDelegate(string msg);
         public delegate int IntDelegate();
         public delegate T add<T>(T a, T b);
-        static void test()
-        {
-
-        }
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
